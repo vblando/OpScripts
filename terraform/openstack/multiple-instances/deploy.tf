37b921ef2adb1a547906d8b27e97fe2eb7ef6436
Vladimir Franciz S. Blando <vladimir.blando@gmail.com>
2020-05-08 23:19:13 +0800

    updated to conform with latest terraform version

--- terraform/openstack/multiple-instances/deploy.tf
+++ terraform/openstack/multiple-instances/deploy.tf
@@ -1,24 +1,22 @@
-resource "openstack_compute_instance_v2" "<instance_name>" {
-  count = "${var.count}"
-  name = "<instance_name>${count.index + 1}"
-  image_name = "${var.image}"
-  availability_zone = "${var.availability_zone}"
-  flavor_name = "${var.flavor}"
-  key_pair = "${var.ssh_key_pair}"
-  security_groups = ["${var.security_group}"]
+  count                         = var.qty
+  name                          = format("%s-%02d", var.instance_name, count.index+1)
+  availability_zone             = var.availability_zone
+  flavor_name                   = var.flavor_name
+  key_pair                      = var.ssh_keypair
+  security_groups               = [var.secgroup]
+  image_name                    = var.image
   network {
-    name = "${var.network}"
+    name                        = var.tenant_network
   }
 }
 
-# get public network name with "openstack network list"
 resource "openstack_networking_floatingip_v2" "instance_fip" {
-  count = "${var.count}"
-  pool = "public_network"
+  count                         = var.qty
+  pool                          = var.external_network
 }
 
 resource "openstack_compute_floatingip_associate_v2" "instance_fip" {
-  count = "${var.count}"
-  floating_ip = "${element(openstack_networking_floatingip_v2.instance_fip.*.address, count.index)}"
-  instance_id = "${element(openstack_compute_instance_v2.<instance_name.*.id, count.index)}"
+  count                         = var.qty
+  floating_ip                   = "${openstack_networking_floatingip_v2.instance_fip.*.address[count.index]}"
+  instance_id                   = "${openstack_compute_instance_v2.Instance.*.id[count.index]}"
 }
